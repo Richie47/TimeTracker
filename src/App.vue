a1de8966e6371dc9f0a681129e5a78cdbb1ad58b
Richie47 <richiemck47@gmail.com>
2020-02-24 23:55:06 -0700

    tutorial progress

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div id="app" class="small-container">
+   <h1>Employees</h1>
+    <employee-table />
   </div>
 </template>
-
 <script>
+import EmployeeTable from './components/EmployeeTable'
 export default {
-  name: 'App'
+  name: 'app',
+  components: {EmployeeTable}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
 </style>
